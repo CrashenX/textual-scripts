58565094a12be0a90b52e395c7709ad1c599d46a
Jesse J. Cook <jesse.j.cook@member.fsf.org>
2016-10-14 18:11:05 +0000

    added osick script

---
+++ osick.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+
+team = [ "lone-rager",
+        "hurricanerix",
+        "JayF",
+        "jroll",
+        "mariojv",
+        "Nadeem",
+        "rosmaita",
+        ]
+
+try:
+    text = " ".join(sys.argv[2:])
+except IndexError:
+    text = ""
+
+print("%s: %s" % (" ".join(team), text))
